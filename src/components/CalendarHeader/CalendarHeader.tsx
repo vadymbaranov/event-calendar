d94f832d14f495158a98490c95ed5b24b8b91317
Vadym Baranov <vadym.baranov@hotmail.com>
2023-01-30 10:25:05 +0200

    Header positioning fixed

--- src/components/CalendarHeader/CalendarHeader.tsx
+++ src/components/CalendarHeader/CalendarHeader.tsx
@@ -19,44 +19,47 @@ export const CalendarHeader: React.FC<Props> = ({
   onToday,
 }) => {
   return (
-    <div className="content">
-      <button
-        type="button"
-        className="button is-link is-outlined"
-        onClick={() => {
-          onFormShown(!isFormShown);
-        }}
-      >
-        +
-      </button>
+    <div className="content is-justify-content-space-between">
+      <div className="container">
+        <button
+          type="button"
+          className="button is-link is-outlined"
+          onClick={() => {
+            onFormShown(!isFormShown);
+          }}
+        >
+          +
+        </button>
+        <div className="switcher__container">
+          <button
+            type="button"
+            className="button is-primary is-outlined"
+            onClick={onToday}
+          >
+            Today
+          </button>
 
-      <button
-        type="button"
-        className="button is-primary is-outlined"
-        onClick={onToday}
-      >
-        Today
-      </button>
-
-      <button
-        type="button"
-        className="button is-primary is-outlined"
-        onClick={onPrevMonth}
-      >
-        {'<'}
-      </button>
-      <h2 className="subtitle is-3">
-        {today.format('MMMM')}
-        &nbsp;
-        {today.format('YYYY')}
-      </h2>
-      <button
-        type="button"
-        className="button is-primary is-outlined"
-        onClick={onNextMonth}
-      >
-        {'>'}
-      </button>
+          <button
+            type="button"
+            className="button is-primary is-outlined"
+            onClick={onPrevMonth}
+          >
+            {'<'}
+          </button>
+          <h2 className="subtitle is-4">
+            {today.format('MMMM')}
+            &nbsp;
+            {today.format('YYYY')}
+          </h2>
+          <button
+            type="button"
+            className="button is-primary is-outlined"
+            onClick={onNextMonth}
+          >
+            {'>'}
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
